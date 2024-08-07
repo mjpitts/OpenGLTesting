d487a49a15dede46378f7f7a258723f2f7a31542
Michael Pitts <michaeljeffreypitts@gmail.com>
2024-08-07 16:11:41 -0700

    All the code up to Episode 4 in the openGl playlist.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
 
 int main(void)
 {
@@ -18,6 +21,12 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+        std::cout << "glew init error" << std::endl;
+
+    /* Print openGl version */
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
