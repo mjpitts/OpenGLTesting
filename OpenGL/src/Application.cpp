9a85ffbe15ee1df87a3311b4b9dc45c9d28b3aa5
Michael Pitts <michaeljeffreypitts@gmail.com>
2024-08-09 20:45:44 -0700

    Add initial shader, will switch to actual shader files next

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -3,9 +3,55 @@
 #include <iostream>
 
 // Episode 7: Writing shaders
-static int Createshader(const std::string& vertexShader, const std::string& fragmentShader)
+
+static unsigned int CompilerShader(unsigned int type, const std::string& source)
 {
+   unsigned int id = glCreateShader(type);
+   const char* src = source.c_str();
+   glShaderSource(id, 1, &src, nullptr);
+   glCompileShader(id);
+
+   /* Error handling. */
+   int result;
+   glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+   if (result == GL_FALSE) 
+   {
+       int length;
+       glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+       char* message = (char*)alloca(length * sizeof(char));
+       glGetShaderInfoLog(id, length, &length, message);
+       std::cout << "Failed to Compile shader " << std::endl;
+       std::cout << message << std::endl;
+
+       /* Delete failed shader and return 0. */
+       glDeleteShader(id);
+       return 0;
+   }
+
+   return id;
+}
+
+/* Provide openGl with a shader source code and text, want openGl to complie and link, then give unique to bind, link the buffer. */
+static unsigned int Createshader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
     
+    /* Think of these as files that need to be linked. */
+    unsigned int vs = CompilerShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompilerShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    /* Attach shaders to program. */
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    /* Get rid of shaders now that they are part of the program. */
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
 }
 // Episode 7 -----------------------
 
@@ -55,6 +101,34 @@ int main(void)
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
     // Episode 4: --------------------------------------
     
+    // Episode 7 ---------------------
+
+    /* Shader source code. */
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(0.5, 0.0, 0.5, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = Createshader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+    // Episode 7 ---------------------
+
     // Episode 5: Vertex attributes --------------------
 
     /* Define vertex position attribute. */
@@ -78,6 +152,9 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    /* Clean up shader. */
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
